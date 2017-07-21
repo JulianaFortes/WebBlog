99e381329b73ec30393c7fa92df59d8fd67c56d0
JulianaFortes <julianasameiro@gmail.com>
2017-07-21 19:38:58 +0100

    Blog Pronto

--- blog/index.js
+++ blog/index.js
@@ -1,4 +1,6 @@
 var bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer"), //Isto é para não deixar o user introduzir scripts ao escrever a informação
     mongoose = require("mongoose"),
     express = require("express"),
     app = express();
@@ -9,6 +11,8 @@ mongoose.connect("mongodb://localhost/blog");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer()); //O sanitizer tem que vir depois do body-parser. Neste site não vou chegar a utilizar isto.
+app.use(methodOverride("_method")); //Isto serve para pudermos fazer o PUT request para fazermos update de uma edição do formulário
 
 
 // MONGOOSE/MODEL CONFIG
@@ -22,12 +26,13 @@ var blogSchema = new mongoose.Schema({
 var Blog = mongoose.model("Blog", blogSchema);
 
 
-//RESTFUL ROUTES
+//-----------RESTFUL ROUTES------------------------
 
 app.get("/", function(req, res) {
     res.redirect("/blogs");
 });
 
+//INDEX ROUTE
 app.get("/blogs", function(req, res){
     Blog.find({}, function(err, blogs){
         if(err){
@@ -38,6 +43,75 @@ app.get("/blogs", function(req, res){
     });
    
 });
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+    //create blog
+    Blog.create(req.body.blog, function(err, newBlog){  //Aqui é req.body.blog porque no new.ejs, no form, pus o nome blog[...] por isso ele assume o title, image e body automaticamente
+        if(err){
+            res.render("new");
+        }else{
+            //then, redirect to the index
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){  //A palavra inicial "Blog" é para o mongodb
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    })
+});
+
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if (err){
+            err.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});      
+        }
+    });
+});
+
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if (err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    });
+});
+
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    //Destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+
+});
+
+
+
+
 
 
 app.listen(process.env.PORT, process.env.IP, function(){
